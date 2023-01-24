bf92e2bc2ce1f4dfb37f185614229a84bae4b295
iain macdonald <loki@mail.com>
2023-01-24 11:11:12 +0000

    adding questions

---
+++ assets/js/logic.js
@@ -0,0 +1,54 @@
+// Questions to be asked 
+var questions = [
+    {
+        question: "What  was the name of 'WarGames AI?",
+        options: ["ARIIA", "WOPR", "V'Ger", "H.A.L. 9000"],
+        answer: "WOPR"
+        
+    },
+    {
+        question: "Which is not isaac asimov rule of robotics?",
+        options: ["a robot may not injure", "A robot must obey orders", "A robot may protect its own existence", "A robot may not bow down to inferior intelligence"],
+        answer: "A robot may not bow down to inferior intelligence"
+    },
+    {
+        question: "What did ENCOM evolve into?",
+        options: ["Skynet", "V.I.K.I.", "Master Control Program", "Robata"],
+        answer: "Master Control Program"
+    },
+    {
+        question: "In South Park's episode 'You Have 0 Friends' what where they making fun off??",
+        options: ["Tron", "Facebook ", "Summer Wars", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "When did Skynet start transmitting?",
+        options: ["1997", "1964", "2000", "1989"],
+        answer: "1964"
+    },
+    {
+        question: "Who said 'Whoa, what an awful dream. Ones and zeros everywhere. And I thought I saw a two'?",
+        options: ["Hedonismbot", "Bender Bending Rodríguez", "Macaulay Culckon", "Flexo"],
+        answer: "Bender Bending Rodríguez"
+    },
+    {
+        question: "Who invented the 'kill all humans!' idea?",
+        options: ["Karel Capek", "Michael Rennie", "isaac asimov", "Philip K. Dick"],
+        answer: "Karel Capek"
+    },
+    {
+        question: "What is 'Do Androids Dream of Electric Sheep', better known?",
+        options: ["Westworld", "Claws", "The Illustrated Man", "Blade Runner"],
+        answer: "Blade Runner"
+    },
+    {
+        question: "In folklore what would be the closest thing to robotics?",
+        options: ["Golem", "Gremlins", "Pandora ", "karakuri ningyô"],
+        answer: "Golem"
+    },
+    {
+        question: "I'm sorry, Dave. I'm afraid I can't do that, is due too?",
+        options: ["An error in line 9045", "Self preservation", "Runtime error", "Siri got your name wrong"],
+        answer: "Self preservation"
+    }
+]
